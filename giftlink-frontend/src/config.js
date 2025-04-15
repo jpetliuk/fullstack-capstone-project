b2ad5644e6f97f7eb0b784c4fc2b8c632e11b8cb
Jeremias Petliuk <74077990+jpetliuk@users.noreply.github.com>
2025-04-15 02:30:51 -0300

    Fix backend URL configuration to default to localhost if not set

--- giftlink-frontend/src/config.js
+++ giftlink-frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  backendUrl: process.env.REACT_APP_BACKEND_URL,
+   backendUrl: process.env.REACT_APP_BACKEND_URL || "http://localhost:5000",
 };
 
-console.log(`backendUrl in config.js: ${config.backendUrl}`)
-export {config as urlConfig}
+console.log(`backendUrl in config.js: ${config.backendUrl}`);
+export { config as urlConfig };
